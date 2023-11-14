6633b4e7a655242127833b70f0cd0763658bec61
Bogdan Caleta Ivkovic <bogdan.caleta@gmail.com>
2023-11-14 01:46:24 +0100

    load reservation into ui

--- controllers/EditReservationController.php
+++ controllers/EditReservationController.php
@@ -1,7 +1,8 @@
 <?php
 
 
+$reservation = $reservation_worker->getReservation(str_replace('/admin/reservation-edit/', '', $_SERVER['REQUEST_URI']));
 
-echo $twig->render('admin/panel/edit_reservation.html.twig', ['page' => $request_uri]);
+echo $twig->render('admin/panel/edit_reservation.html.twig', ['page' => $request_uri, 'reservation'=>$reservation]);
 
 ?>
